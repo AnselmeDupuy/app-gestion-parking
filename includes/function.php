1d9a9716ad06d3ba372d0113efae207f0666942b
anselme <anselmedupuy@gmail.com>
2025-05-12 18:31:00 +0200

    add checkUser

--- includes/function.php
+++ includes/function.php
@@ -5,6 +5,19 @@ function cleanString(string $value): string
         return trim(htmlspecialchars($value, ENT_QUOTES));
     }
 
+function isAdmin(): bool
+    {
+        return isset($_SESSION['admin']);
+    }
+    
+function isUser(): bool
+    {
+        return isset($_SESSION['user']);
+    }
 
+function isGuest(): bool
+    {
+        return !isset($_SESSION['user']) && !isset($_SESSION['admin']);
+    }
 
 ?>
